aa4624d3fa1c78ab184ae38d5ffb955b961b4c46
MATEUS RODRIGUES DA SILVA <mateus.rsilva30@senacsp.edu.br>
2023-03-02 22:12:27 -0300

    repositorio4

--- UC9_NOTAS2/Program.cs
+++ UC9_NOTAS2/Program.cs
@@ -1,8 +1,4 @@
-﻿string aluno1;
-string aluno2;
-string aluno3;
-string aluno4;
-string aluno5;
+﻿string aluno;
 int i;
 int faltas;
 int aulas;
@@ -14,46 +10,61 @@ double peso1 = 0;
 double peso2 = 0;
 double peso3 = 0;
 double md = 0;
+double presenca = 0;
+string resultado = "";
+
+for (i = 1; i <= 5; i++)
+{
+
+    Console.WriteLine("Informe o nome do " + (i + 1) + "º aluno");
+    aluno = Console.ReadLine();
+
+    Console.WriteLine("Informe a quantidade de aulas");
+    aulas = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Informe a quantidade de faltas");
+    faltas = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Informe a nota da prova 1");
+    prova1 = double.Parse(Console.ReadLine());
+    Console.WriteLine("Digite o peso para a nota da prova 1");
+    peso1 = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Informe a nota da prova 2");
+    prova2 = double.Parse(Console.ReadLine());
+    Console.WriteLine("Digite o peso para a nota da prova 2");
+    peso2 = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Informe a nota da prova 3");
+    prova3 = double.Parse(Console.ReadLine());
+    Console.WriteLine("Digite o peso para a nota da prova 3");
+    peso3 = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Informe a nota do trabalho");
+    trabalho = double.Parse(Console.ReadLine());
+
+    md = (prova1 * peso1) + (prova2 * peso2) + (prova3 * peso3);
+    presenca = (faltas / aulas) * 100;
 
 
-Console.WriteLine("Informe o nome do 1º aluno");
-aluno1 = Console.ReadLine();
 
-Console.WriteLine("Informe o nome do 2º aluno");
-aluno2 = Console.ReadLine();
+    if (md >= 6 && presenca > 25)
+    {
+        resultado = "Aprovado";
+    }
+    if (md < 6 && presenca >= 25)
+    {
+        resultado = "Reprovado por Menção";
+    }
+    if (md > 6 && presenca < 25)
+    {
+        resultado = "Reprovado por Faltas";
+    }
+    if (md < 6 && presenca < 25)
+    {
+        resultado = "Reprovado por Menção e Faltas";
+    }
 
-Console.WriteLine("Informe o nome do 3º aluno");
-aluno3 = Console.ReadLine();
+    Console.WriteLine(aluno + ", sua nota foi de: " + md + " sua presença foi de: " + presenca + ", o Aluno foi ");
 
-Console.WriteLine("Informe o nome do 4º aluno");
-aluno4 = Console.ReadLine();
-
-Console.WriteLine("Informe o nome do 5º aluno");
-aluno5 = Console.ReadLine();
-
-Console.WriteLine("Informe a quantidade de aulas");
-aulas = int.Parse(Console.ReadLine());
-
-Console.WriteLine("Informe a quantidade de faltas");
-faltas = int.Parse(Console.ReadLine());
-
-Console.WriteLine("Informe a nota da prova 1");
-prova1 = double.Parse(Console.ReadLine());
-Console.WriteLine("Digite o peso para a nota da prova 1");
-peso1 = double.Parse(Console.ReadLine());
-
-Console.WriteLine("Informe a nota da prova 2");
-prova2 = double.Parse(Console.ReadLine());
-Console.WriteLine("Digite o peso para a nota da prova 2");
-peso2 = double.Parse(Console.ReadLine());
-
-Console.WriteLine("Informe a nota da prova 3");
-prova3 = double.Parse(Console.ReadLine());
-Console.WriteLine("Digite o peso para a nota da prova 3");
-peso3 = double.Parse(Console.ReadLine());
-
-Console.WriteLine("Informe a nota do trabalho");
-trabalho = double.Parse(Console.ReadLine());
-
-md = (prova1 * peso1) + (prova2 * peso2) + (prova3 * peso3);
-
+}
